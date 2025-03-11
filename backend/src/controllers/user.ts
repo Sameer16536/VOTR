2f5659241e24068e8a5723ec9724257ca1a0bd04
Sameer Marathe <sameer.marathe16536@sakec.ac.in>
2025-03-11 23:04:41 +0530

    add user registration and presigned URL functionality

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,4 +1,57 @@
-import { Request,Response } from "express"
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
 
+const prisma = new PrismaClient();
+const JWT_SECRET = process.env.JWT_SECRET!;
 
-export const registerUser = async (req: Request, res: Response) => {}
+export const registerUser = async (req: Request, res: Response) => {
+  const hardcodedWalletAddress = "0x1234567890";
+
+  // If no user exists then create a new user using upsert , if exists then update
+  // const user = await prisma.user.upsert()
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      address: hardcodedWalletAddress,
+    },
+  });
+
+  if (existingUser) {
+    const token = jwt.sign(
+      {
+        userId: existingUser.id,
+      },
+      JWT_SECRET
+    );
+
+    res.json({
+      token,
+      msg: "Token generated successfully for existing user",
+    });
+  } else {
+    const newUser = await prisma.user.create({
+      data: {
+        address: hardcodedWalletAddress,
+      },
+    });
+
+    const token = jwt.sign(
+      {
+        userId: newUser.id,
+      },
+      JWT_SECRET
+    );
+    res.json({
+      token,
+      msg: "Token generated successfully for new user",
+    });
+  }
+};
+
+export const getPresignedUrl = async (req: Request, res: Response) => { 
+
+}
+
--- backend/src/routers/user.ts
+++ backend/src/routers/user.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { registerUser } from "../controllers/user";
+import { getPresignedUrl, registerUser } from "../controllers/user";
 
 const router = Router();
 
 router.post('/signin',registerUser)
+router.get('/presignedUrl',getPresignedUrl)
 
 export default router;
